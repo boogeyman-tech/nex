6930d86824397a4d7054bb0f29a1def9a7e1daa5
boogeyman-tech <boogeyman9495@gmail.com>
2025-11-19 18:38:31 +0000

    successful scanning and recent vulnerbilities module is scrollable

--- intrunex/bin/worker-run.sh
+++ intrunex/bin/worker-run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+php bin/console messenger:consume async -vv
+
 while true; do
   php bin/console messenger:consume async --memory-limit=128M --time-limit=3600 -vvv
   echo "Worker crashed with exit code $?. Respawning in 5 seconds..."
