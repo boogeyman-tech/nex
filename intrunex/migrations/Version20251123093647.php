26432a080c00c10f50e5443d112abb5cbfefface
boogeyman-tech <boogeyman9495@gmail.com>
2025-11-23 09:48:22 +0000

    updated the registration page to accept more data

---
+++ intrunex/migrations/Version20251123093647.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20251123093647 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE user ADD COLUMN name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD COLUMN country_code VARCHAR(10) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD COLUMN mobile_number VARCHAR(20) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD COLUMN organization_name VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD COLUMN job_role VARCHAR(100) DEFAULT NULL');
+        $this->addSql('ALTER TABLE user ADD COLUMN job_description VARCHAR(500) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__user AS SELECT id, email, roles, password FROM user');
+        $this->addSql('DROP TABLE user');
+        $this->addSql('CREATE TABLE user (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, email VARCHAR(255) NOT NULL, roles CLOB DEFAULT NULL, password VARCHAR(255) NOT NULL)');
+        $this->addSql('INSERT INTO user (id, email, roles, password) SELECT id, email, roles, password FROM __temp__user');
+        $this->addSql('DROP TABLE __temp__user');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_8D93D649E7927C74 ON user (email)');
+    }
+}
